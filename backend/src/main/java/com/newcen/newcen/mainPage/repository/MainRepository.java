4559d1a67d95545a39ba5a8d6ea8a7f9a398b66a
dlwlsgur0909 <dlwlsgur0909@naver.com>
2023-02-06 17:23:10 +0900

    MainRepository 생성

--- backend/src/main/java/com/newcen/newcen/mainPage/repository/MainRepository.java
+++ backend/src/main/java/com/newcen/newcen/mainPage/repository/MainRepository.java
@@ -1,5 +1,6 @@
 package com.newcen.newcen.mainPage.repository;
 
+import com.newcen.newcen.common.entity.BoardEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MainRepository extends JpaRepository<BoardEntity, Long> {
