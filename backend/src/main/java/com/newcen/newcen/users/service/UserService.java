98ad845a1fc857b9a3906658a135eb3406a7556c
Dev-Jinyeong <yeong2678@gmail.com>
2023-02-07 17:20:41 +0900

    feat: UserService 개발필요

--- backend/src/main/java/com/newcen/newcen/users/service/UserService.java
+++ backend/src/main/java/com/newcen/newcen/users/service/UserService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
-@RequiredArgsConstructor
+@RequiredArgsConstructor    // 초기화 되지않은 final 또는 @NonNull 이 붙은 필드에 대해 생성자를 생성
 public class UserService {
 
 
