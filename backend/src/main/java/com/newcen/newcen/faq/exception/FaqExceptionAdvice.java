61dae05bbca2378c17bd35edfbabd2ceaff14373
dlwlsgur0909 <dlwlsgur0909@naver.com>
2023-02-10 11:02:56 +0900

    feat: Message / FAQ 기능구현 완료

--- backend/src/main/java/com/newcen/newcen/faq/exception/FaqExceptionAdvice.java
+++ backend/src/main/java/com/newcen/newcen/faq/exception/FaqExceptionAdvice.java
@@ -1,12 +1,22 @@
 package com.newcen.newcen.faq.exception;
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException;
 import com.newcen.newcen.message.exception.MessageCustomException;
 import com.newcen.newcen.message.exception.MessageExceptionEntity;
 import com.newcen.newcen.message.exception.MessageExceptionEnum;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingPathVariableException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
+
+import java.net.BindException;
 
 @RestControllerAdvice(basePackages = "com.newcen.newcen.faq")
 public class FaqExceptionAdvice {
@@ -21,6 +31,28 @@ public class FaqExceptionAdvice {
                         .build()
                 );
     }
+
+    @ExceptionHandler({
+            MissingPathVariableException.class,
+            MethodArgumentNotValidException.class,
+            MissingServletRequestParameterException.class,
+            HttpClientErrorException.BadRequest.class,
+            InvalidFormatException.class,
+            DuplicateKeyException.class,
+            IllegalArgumentException.class,
+            DataIntegrityViolationException.class,
+            HttpMessageNotReadableException.class,
+            BindException.class
+    })
+    public ResponseEntity<?> invalidParameterHandler(Exception e) {
+        return ResponseEntity.status(FaqExceptionEnum.INVALID_PARAMETER.getStatus())
+                .body(FaqExceptionEntity.builder()
+                        .errorCode(FaqExceptionEnum.INVALID_PARAMETER.getCode())
+                        .errorMessage(FaqExceptionEnum.INVALID_PARAMETER.getMessage())
+                        .build()
+                );
+    }
+
 
     @ExceptionHandler({FaqCustomException.class})
     public ResponseEntity<?> faqCustomExceptionHandler(FaqCustomException ce) {
