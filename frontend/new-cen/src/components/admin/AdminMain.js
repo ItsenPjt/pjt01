4b9c11cec0cc33eda0f397ee0176f13c971b7d98
tudfldps99 <tudfldps99@naver.com>
2023-02-13 11:43:12 +0900

    fix: 공지사항,문의사항 추가 로직 수정

--- frontend/new-cen/src/components/admin/AdminMain.js
+++ frontend/new-cen/src/components/admin/AdminMain.js
@@ -64,12 +64,10 @@ const AdminMain = () => {
                 <thead>
                     <tr>
                         <th width="2%"></th>
-                        <th width="10%">권한</th>       {/* 해당 직원이 관리자(O)인지 사용자(X)인지 */}
+                        <th width="5%">권한</th>       {/* 해당 직원이 관리자(O)인지 사용자(X)인지 */}
                         <th width="6%">이름</th>
                         <th width="6%">아이디</th>
-                        <th width="6%">인증코드</th>
-                        <th width="10%">전화번호</th>
-                        <th width="8%">정보 변경</th>
+                        <th width="6%">가입일</th>
                         <th width="8%">퇴사 처리</th>
                     </tr>
                 </thead>
@@ -83,9 +81,7 @@ const AdminMain = () => {
                         <td>X</td>
                         <td>암호맨</td>
                         <td>postman@naver.com</td>
-                        <td>XY2baJQ</td>
-                        <td>010-0000-0000</td>
-                        <td><Button className="btn_gray" onClick = {() => handleChangeModal('암호맨userId')}>변경</Button></td>
+                        <td>2023-01-09</td>
                         <td><Button className="btn_orange" onClick = {() => handleDeleteModal('암호맨userId')}>퇴사</Button></td>
                     </tr>
                 </tbody>
