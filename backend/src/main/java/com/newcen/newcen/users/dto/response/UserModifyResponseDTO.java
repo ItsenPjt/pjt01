69dfc989a87e0ea7ae493a586cba7cb4790571fc
Dev-Jinyeong <yeong2678@gmail.com>
2023-02-14 16:42:51 +0900

    feat: User Info Modify - [BE]

--- backend/src/main/java/com/newcen/newcen/users/dto/response/UserModifyResponseDTO.java
+++ backend/src/main/java/com/newcen/newcen/users/dto/response/UserModifyResponseDTO.java
@@ -15,25 +15,7 @@ import java.time.LocalDate;
 @EqualsAndHashCode
 public class UserModifyResponseDTO {
 
-    private String userEmail;
-    private String userName;
-
-    @JsonFormat(pattern = "yyyy년 MM월 dd일")
-    private LocalDate userRegdate;
-
-    private String token;   // 인증 토큰
-
     private String message; // 응답 메세지
 
-
-    // 엔터티를 DTO로 변경
-    public UserModifyResponseDTO(UserEntity entity) {
-
-        this.userEmail = entity.getUserEmail();
-        this.userName = entity.getUserName();
-        this.userRegdate = entity.getUserRegdate();
-        this.token = getToken();
-
-    }
 
 }
