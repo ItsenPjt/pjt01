6a70622a66e608f0169cda5a4aac5ab2a53da669
HangHang13 <dhkdwlsgod@naver.com>
2023-02-16 13:44:03 +0900

    fix : casecade 설정

--- backend/src/main/java/com/newcen/newcen/commentFile/repository/CommentFileRepository.java
+++ backend/src/main/java/com/newcen/newcen/commentFile/repository/CommentFileRepository.java
@@ -3,5 +3,11 @@ package com.newcen.newcen.commentFile.repository;
 import com.newcen.newcen.common.entity.CommentFileEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentFileRepository extends JpaRepository<CommentFileEntity, String> {
+
+
+
+
 }
